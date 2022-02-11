64220e9f682b3afa4331da06d37bad2be1be43b2
unknown <mariana.avilesmartinez@gmail.com.com>
2022-02-11 15:08:45 -0600

    Botones agregados 2

--- ejercicio1/js/interaccion.js
+++ ejercicio1/js/interaccion.js
@@ -16,6 +16,8 @@ boton1 = document.getElementById('boton1');
 boton2 = document.getElementById('boton2');
 boton3 = document.getElementById('boton3');
 
+
+
 if(boton1.disabled == false){
     boton2.disabled = true;
     boton3.disabled = true;
@@ -23,30 +25,20 @@ if(boton1.disabled == false){
     boton3.textContent = 'Desactivado';
 }
 
-boton1.addEventListener("click", boton1off);
-boton2.addEventListener("click", boton2off);
-boton3.addEventListener("click", boton3off);
+boton1.addEventListener("click", function() {
+    Desactivar(boton1, boton2);
+});
 
-function boton1off(){
-    boton1.disabled = true;
-    boton2.disabled = false;
-    boton1.textContent = 'Desactivado';
-    boton2.textContent = 'Presioname!!!';
+boton2.addEventListener("click", function() {
+    Desactivar(boton2, boton3);
+});
+boton3.addEventListener("click", function() {
+    Desactivar(boton3, boton1);
+});
+
+function Desactivar(b1,b2){
+    b1.disabled = true;
+    b2.disabled = false;
+    b1.textContent = 'Desactivado';
+    b2.textContent = 'Presioname!!!';
 }
-
-function boton2off(){
-    boton2.disabled = true;
-    boton3.disabled = false;
-    boton2.textContent = 'Desactivdado';  
-    boton3.textContent = 'Presioname!!!';
-}
-
-function boton3off(){
-    boton3.disabled = true;  
-    boton1.disabled = false;
-    boton3.textContent = 'Desactivado';
-    boton1.textContent = 'Presioname!!!';
-}
-
-
-
